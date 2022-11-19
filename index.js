24b75cd108b981502a0c79fac4916e1bc3d00827
Ryan P <rdpinkstaff@gmail.com>
2022-11-19 16:20:57 -0700

    Fully working site

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
-
 const server = http.createServer((req, res) => {
-  
   const filePath = path.join(
     __dirname,
     "public",
@@ -12,12 +10,22 @@ const server = http.createServer((req, res) => {
   );
 
   fs.readFile(filePath, (err, content) => {
-    if (err) throw err;
-    console.log(err)
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(content, "utf-8");
-
-
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf-8");
+          }
+        );
+      } else {
+        res.end(`${err.code} Server error...`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf-8");
+    }
   });
 });
 
